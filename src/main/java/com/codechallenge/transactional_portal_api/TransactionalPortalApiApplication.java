7a5a78a5d67cc3e4eecd43e9b4396445030ffe05
jonathan Tellez <jtellez@itroi.co>
2025-11-04 18:42:07 -0500

    chore: start project

---
+++ src/main/java/com/codechallenge/transactional_portal_api/TransactionalPortalApiApplication.java
@@ -0,0 +1,13 @@
+package com.codechallenge.transactional_portal_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalPortalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionalPortalApiApplication.class, args);
+    }
+
+}
