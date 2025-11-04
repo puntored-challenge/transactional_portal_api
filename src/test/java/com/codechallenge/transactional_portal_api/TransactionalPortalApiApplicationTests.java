2c5f6c6178356d7c857b257bd3e853a064162cdc
jonathan Tellez <jtellez@itroi.co>
2025-11-04 18:42:07 -0500

    chore: start project

---
+++ src/test/java/com/codechallenge/transactional_portal_api/TransactionalPortalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codechallenge.transactional_portal_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionalPortalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
